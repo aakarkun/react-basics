f5728d5656872a7f0049b81fa9b288fb3e6402ea
aakarkun <aakar.kun@hotmail.com>
2017-06-08 08:20:57 +0545

    use of props in Home Component

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -1,11 +1,33 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export class Home extends React.Component {
   render() {
     return (
       <div>
-        I am from Home component!
+        <h4>User Information</h4>
+        <h1>{this.props.name}</h1>
+        <p>{this.props.email}</p>
+        <p>Studying {this.props.college.faculty} at {this.props.college.name}, located at {this.props.college.address}.</p>
+        <h5>Skills</h5>
+        <h6>Frontend</h6>
+          <ul>
+            {this.props.skills.frontend.map((frontend, i) => <li key={i}>{frontend}</li>)}
+          </ul>
+        <h6>Backend</h6>
+        <ul>
+          {this.props.skills.backend.map((backend, i) => <li key={i}>{backend}</li>)}
+        </ul>
+        {this.props.children}
       </div>
     );
   }
+}
+
+Home.propTypes = {
+  name: PropTypes.string,
+  email: PropTypes.string,
+  college: PropTypes.object,
+  skills: PropTypes.object,
+  children: PropTypes.isRequired
 }
--- src/app/index.js
+++ src/app/index.js
@@ -6,6 +6,15 @@ import { Home } from './components/Home';
 
 class App extends React.Component {
   render() {
+    var college = {
+      name: "New Summit College",
+      address: "Old Baneshwor, Kathmandu",
+      faculty: "Bsc.CSIT"
+    };
+    var skills = {
+      frontend: ["HTML/CSS", "JavaScript", "ReactJS"],
+      backend: ["JavaP", "PHP", "NodeJS"]
+    };
     return (
       <div className="container">
         <div className="row">
@@ -15,7 +24,9 @@ class App extends React.Component {
         </div>
         <div className="row">
           <div className="col-xs-10 col-xs-offset-1">
-            <Home />
+            <Home name={"Kus Gautam"} age={21} college={college} skills={skills} email="kusgautam@hotmail.com">
+              <p>Thank you! Hope you will love React JS!</p>
+            </Home>
           </div>
         </div>
       </div>
